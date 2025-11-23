53577614ed842732521455958af918b5810050a5
DjX <karwowski.d@o2.pl>
2025-11-23 13:03:20 +0100

    Add step 2 solution

---
+++ src/dns_message.rs
@@ -0,0 +1,188 @@
+﻿pub struct DnsMessage {
+    pub header: Header,
+}
+
+pub struct Header {
+    pub packet_id: u16,
+    pub qr_ind: QueryResponseIndicator,
+    pub op_code: OperationCode,
+    pub is_auth_ans: bool,
+    pub is_trunc: bool,
+    pub is_rec_desired: bool,
+    pub is_rec_available: bool,
+    pub r_code: ResponseCode,
+    pub qd_count: u16,
+    pub an_count: u16,
+    pub ns_count: u16,
+    pub ar_count: u16,
+}
+
+pub enum QueryResponseIndicator {
+    Query,
+    Response,
+}
+
+pub enum OperationCode {
+    Query,
+    IQuery,
+    Status,
+}
+
+pub enum ResponseCode {
+    NoError,
+    FormatError,
+    ServerFailure,
+    NameError,
+    NotImplemented,
+    Refused,
+}
+
+impl DnsMessage {
+    pub fn deserialize(buf: &[u8; 512]) -> DnsMessage {
+        let header = Header::deserialize(buf);
+
+        DnsMessage { header }
+    }
+
+    pub fn serialize(&self) -> [u8; 512] {
+        let mut msg: [u8; 512] = [0; 512];
+
+        msg[..12].copy_from_slice(&self.header.serialize());
+
+        msg
+    }
+}
+
+impl Header {
+    fn deserialize(buf: &[u8; 512]) -> Header {
+        Header {
+            packet_id: u16::from_be_bytes(
+                buf[..2].try_into().expect("Failed to create packet_id."),
+            ),
+            qr_ind: Self::deserialize_qr_ind(buf),
+            op_code: Self::deserialize_op_code(buf),
+            is_auth_ans: get_bit_flag_for_byte(buf, 2, 5),
+            is_trunc: get_bit_flag_for_byte(buf, 2, 6),
+            is_rec_desired: get_bit_flag_for_byte(buf, 2, 7),
+            is_rec_available: get_bit_flag_for_byte(buf, 3, 0),
+            r_code: Self::deserialize_r_code(buf),
+            qd_count: u16::from_be_bytes(buf[4..6].try_into().expect("Failed to create qd_count")),
+            an_count: u16::from_be_bytes(buf[6..8].try_into().expect("Failed to create an_count")),
+            ns_count: u16::from_be_bytes(buf[8..10].try_into().expect("Failed to create ns_count")),
+            ar_count: u16::from_be_bytes(
+                buf[10..12].try_into().expect("Failed to create ar_count"),
+            ),
+        }
+    }
+
+    fn serialize(&self) -> [u8; 12] {
+        let mut header: [u8; 12] = [0; 12];
+
+        header[..2].copy_from_slice(&self.packet_id.to_be_bytes());
+
+        let qr_ind = Self::serialize_qr_ind(&self.qr_ind);
+        let op_code = Self::serialize_op_code(&self.op_code);
+        let is_auth_ans = self.is_auth_ans.as_bit_flag(5);
+        let is_trunc = self.is_trunc.as_bit_flag(6);
+        let is_rec_desired = self.is_rec_desired.as_bit_flag(7);
+        header[2] = qr_ind | op_code | is_auth_ans | is_trunc | is_rec_desired;
+
+        let is_rec_available = self.is_rec_available.as_bit_flag(0);
+        let r_code = Self::serialize_r_code(&self.r_code);
+        header[3] = is_rec_available | r_code;
+
+        header[4..6].copy_from_slice(&self.qd_count.to_be_bytes());
+        header[6..8].copy_from_slice(&self.an_count.to_be_bytes());
+        header[8..10].copy_from_slice(&self.ns_count.to_be_bytes());
+        header[10..12].copy_from_slice(&self.ar_count.to_be_bytes());
+
+        header
+    }
+
+    fn deserialize_qr_ind(buf: &[u8; 512]) -> QueryResponseIndicator {
+        match buf[2] & 1 == 1 {
+            false => QueryResponseIndicator::Query,
+            true => QueryResponseIndicator::Response,
+        }
+    }
+
+    fn serialize_qr_ind(qr_ind: &QueryResponseIndicator) -> u8 {
+        match qr_ind {
+            QueryResponseIndicator::Query => 0,
+            QueryResponseIndicator::Response => 1,
+        }
+    }
+
+    fn deserialize_op_code(buf: &[u8; 512]) -> OperationCode {
+        match buf[2] >> 1 {
+            0 => OperationCode::Query,
+            1 => OperationCode::IQuery,
+            2 => OperationCode::Status,
+            _ => panic!("Unexpected OPCODE value"),
+        }
+    }
+
+    fn serialize_op_code(op_code: &OperationCode) -> u8 {
+        (match op_code {
+            OperationCode::Query => 0,
+            OperationCode::IQuery => 1,
+            OperationCode::Status => 2,
+        }) << 1
+    }
+
+    fn deserialize_r_code(buf: &[u8; 512]) -> ResponseCode {
+        match buf[3] >> 4 {
+            0 => ResponseCode::NoError,
+            1 => ResponseCode::FormatError,
+            2 => ResponseCode::ServerFailure,
+            3 => ResponseCode::NameError,
+            4 => ResponseCode::NotImplemented,
+            5 => ResponseCode::Refused,
+            _ => panic!("Unexpected RCODE value"),
+        }
+    }
+
+    fn serialize_r_code(r_code: &ResponseCode) -> u8 {
+        (match r_code {
+            ResponseCode::NoError => 0,
+            ResponseCode::FormatError => 1,
+            ResponseCode::ServerFailure => 2,
+            ResponseCode::NameError => 3,
+            ResponseCode::NotImplemented => 4,
+            ResponseCode::Refused => 5,
+        }) << 4
+    }
+}
+
+fn get_bit_flag_for_byte(buf: &[u8; 512], byte_idx: usize, bit_idx: u8) -> bool {
+    buf[byte_idx].get_bit_flag(bit_idx)
+}
+
+trait GetBitFlag {
+    fn get_bit_flag(&self, bit_idx: u8) -> bool;
+}
+
+impl GetBitFlag for u8 {
+    fn get_bit_flag(&self, bit_idx: u8) -> bool {
+        if bit_idx > 7 {
+            panic!("Bit index must be between 0 and 7")
+        };
+
+        (self >> bit_idx) & 1 == 1
+    }
+}
+
+trait AsBitFlag {
+    fn as_bit_flag(&self, bit_idx: u8) -> u8;
+}
+
+impl AsBitFlag for bool {
+    fn as_bit_flag(&self, bit_idx: u8) -> u8 {
+        if bit_idx > 7 {
+            panic!("Bit index must be between 0 and 7")
+        };
+
+        let bit: u8 = if *self { 1 } else { 0 };
+        bit << bit_idx
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod dns_message;
+
+use crate::dns_message::*;
 #[allow(unused_imports)]
 use std::net::UdpSocket;
 
@@ -6,12 +9,12 @@ fn main() {
 
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
-    
+
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                let response = create_response(&buf);
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
@@ -23,3 +26,28 @@ fn main() {
         }
     }
 }
+
+fn create_response(buf: &[u8; 512]) -> [u8; 512] {
+    let header = Header {
+        packet_id: 1234,
+        qr_ind: QueryResponseIndicator::Response,
+        op_code: OperationCode::Query,
+        is_auth_ans: false,
+        is_trunc: false,
+        is_rec_desired: false,
+        is_rec_available: false,
+        r_code: ResponseCode::NoError,
+        qd_count: 0,
+        an_count: 0,
+        ns_count: 0,
+        ar_count: 0,
+    };
+
+    let dns_message = DnsMessage { header };
+
+    dns_message.serialize()
+}
+
+
+
+
